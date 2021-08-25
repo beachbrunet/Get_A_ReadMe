b13db262438d0a9edacc7c9575b25042ae7dfedc
Catherine Cox <ccox.034@gmail.com>
2021-08-25 14:06:54 -0600

    Saving work, added requirements and psudo code

--- index.js
+++ index.js
@@ -1,18 +1,52 @@
-// Global Variables
+// TODO: Include packages needed for this application
+// Global Variables.
 const placeHolder = require("placeHolder");
 const theFs = require("fs");
 
-// Created a string for the user to input
-const questions = {
-  type: "input",
-  name: "tittle",
-  message: "The title of your project",
-};
-
-// TODO: Include packages needed for this application
-
-// TODO: Create an array of questions for user input
-const questions = [];
+// TODO: Create an array of questions for user input.
+// Created a string for the user to input.
+const questions = [
+  {
+    type: "input",
+    name: "tittle",
+    message: "The title of your project",
+  },
+  {
+    type: "input",
+    name: "tittle",
+    message: "The title of your project",
+  },
+  {
+    type: "input",
+    name: "tittle",
+    message: "The title of your project",
+  },
+  {
+    type: "input",
+    name: "tittle",
+    message: "The title of your project",
+  },
+  {
+    type: "input",
+    name: "tittle",
+    message: "The title of your project",
+  },
+  {
+    type: "input",
+    name: "tittle",
+    message: "The title of your project",
+  },
+  {
+    type: "input",
+    name: "tittle",
+    message: "The title of your project",
+  },
+  {
+    type: "input",
+    name: "tittle",
+    message: "The title of your project",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
