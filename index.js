6dff4d1b31064fcaab27cb5badebb19f7365c5dc
Catherine Cox <ccox.034@gmail.com>
2021-08-25 13:53:57 -0600

    created string layout

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+// Global Variables
+const placeHolder = require("placeHolder");
+const theFs = require("fs");
+
+// Created a string for the user to input
+const questions = {
+  type: "input",
+  name: "tittle",
+  message: "The title of your project",
+};
+
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
