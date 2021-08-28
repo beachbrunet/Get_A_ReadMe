694fbfbace6e3dde63090746e23f65ba9c982457
Catherine Cox <ccox.034@gmail.com>
2021-08-28 16:32:50 -0600

    updated index.js with the correct licenses and updated a few typos

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-// Requirements
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for information about my application repository
-// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-
-// WHEN I enter my project title
-// THEN this is displayed as the title of the README
-
-// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-
-// WHEN I choose a license for my application from a list of options
-// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-
-// WHEN I enter my GitHub username
-// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-
-// WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-
-// WHEN I click on the links in the Table of Contents
-// THEN I am taken to the corresponding section of the README
-
-// TODO: Include packages needed for this application
-// Global Variables.
 const fs = require("fs");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
@@ -40,13 +15,13 @@ const questions = [
   {
     type: "input",
     message: "Please provide a breif discription of your awesome project.",
-    name: "disciption",
+    name: "discription",
   },
   {
     type: "list",
     name: "license",
     message: "What license do you want to use?",
-    choices: ["MiT", "license", "other license", "#"],
+    choices: ["Apache 2.0 Licene", "MIT", "ILP"],
   },
   {
     //   github
@@ -60,21 +35,15 @@ const questions = [
     name: "email",
     message: "Please provide your email address.",
   },
-  {
-    //   extra empty
-    type: "#",
-    name: "#",
-    message: "#",
-  },
 ];
 
 inquirer.prompt(questions).then((data) => {
   console.log(data);
 });
 
-// TODO: Create a function to write README file
+// // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-  fs.writeFile("fileName", data, (err) => {
+  fs.writeFile("README.md", data, (err) => {
     if (error) {
       console.error(err);
     } else {
@@ -83,7 +52,7 @@ function writeToFile(fileName, data) {
   });
 }
 
-// duplicate outline so I commented it out.
+// Duplicate outline so I commented it out.
 // // TODO: Create a function to initialize app
 // function init() {}
 
